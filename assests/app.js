db83229db835fc681a510bc8c84193f6cc69b649
Minhaj <minhajallsocial@gmail.com>
2022-07-08 11:18:04 +0600

    something

---
+++ assests/app.js
@@ -0,0 +1,65 @@
+
+let foods =['amm','jamm','kathal','kola','lichu','peyara','apple','angur','bangi','tormuz','pepe',];
+
+foods.map( (nibo) =>{
+   if (nibo=='pepe') {
+     console.log(nibo);
+   }
+});
+
+const devs =[
+    {
+       name   :'Minhaj',
+       age    :20,
+       skill  :'PHP',
+       mail   :'minhaj@gmail.com',
+       gender :'male'
+    },
+    
+    {
+       name   :'Mafi',
+       age    :24,
+       skill  :'JS',
+       mail   :'mafi@gmail.com',
+       gender :'female'
+    },
+    
+    {
+       name   :'Munsur',
+       age    :25,
+       skill  :'PYTHON',
+       mail   :'munsur@gmail.com',
+       gender :'male'
+    },
+    
+    {
+       name   :'Ayesha',
+       age    :28,
+       skill  :'PHP',
+       mail   :'ayesha@gmail.com',
+       gender :'female'
+    },
+    
+    {
+       name   :'Milon',
+       age    :30,
+       skill  :'LARAVEL',
+       mail   :'milon@gmail.com',
+       gende  :'male'
+    },
+    
+    {
+       name   :'Matin',
+       age    :35,
+       skill  :'PHP',
+       mail   :'matin@gmail.com',
+       gender :'male'
+    },
+];
+
+
+// devs.map( (data) =>{
+//     if (data.gender=='female') {
+//         console.log(data);
+//     }
+// });
